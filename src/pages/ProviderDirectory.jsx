de9eb9592ef54f72447f576d92db001d8af90543
MindfulwayTP <ryne@mindfulway-therapy.com>
2025-07-09 19:08:06 -0700

    provide avail 7-9

--- src/pages/ProviderDirectory.jsx
+++ src/pages/ProviderDirectory.jsx
@@ -147,7 +147,7 @@ useEffect(() => {
       <h1 className="text-3xl  text-center text-sky-700 mb-2">Meet Our Providers</h1>
       <h3 className="text-lg text-center text-gray-700 mb-6">
         Use the search functions below to find a provider. <br /> To learn more, click on each provider's profile<br />
-        <p className="italic">Availability Updated as of July 2025 (we do our best to update this weekly; please note provider availability may change suddenly)</p>
+        <p className="italic">Availability Updated as of July 9th, 2025 (we do our best to update this weekly; please note provider availability may change suddenly)</p>
       </h3>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-12">
